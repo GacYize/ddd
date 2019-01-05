dd5def1ad0314381b8d4170e63ec117a505cde84
admin <admin@163.com>
2019-01-05 22:17:02 +0800

    333333

--- src/main/java/com/demo/ddd/Main.java
+++ src/main/java/com/demo/ddd/Main.java
@@ -3,5 +3,8 @@ package com.demo.ddd;
 public class Main {
     public static void main(String[] args) {
         System.out.println(123);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
