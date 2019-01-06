5dfa209bf5403942299cef0f9d227cf346deecbe
admin <admin@163.com>
2019-01-06 16:26:00 +0800

    8888

--- src/main/java/com/demo/ddd/Main.java
+++ src/main/java/com/demo/ddd/Main.java
@@ -6,30 +6,59 @@ import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
-        Integer[] arr = {1, 23, 53, 243, 2523, 5};
-        Arrays.sort(arr,Collections.reverseOrder());
-        for (Integer a : arr) {
-            System.out.print(a+">");
-        }
-        System.out.println("---------------");
+        Integer[] arr = {1, 23, 53, 243, 2523, 5,99};
+//        Arrays.sort(arr, Collections.reverseOrder());
+//        for (Integer a : arr) {
+//            System.out.print(a + ">");
+//        }
+//        System.out.println("---------------");
         chooseSort(arr);
+        bubbleSort(arr);
     }
 
 
-        private static void chooseSort ( Integer[] arr){
-            for (int i = 0; i < arr.length - 1; i++) {
-                for (int k = i + 1; k < arr.length; k++) {
-                    if (arr[i] < arr[k]) {
-                        int b = arr[i];
-                        arr[i] = arr[k];
-                        arr[k] = b;
-                    }
+    private static void chooseSort(Integer[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int k = i + 1; k < arr.length; k++) {
+                if (arr[i] > arr[k]) {
+                    int b = arr[i];
+                    arr[i] = arr[k];
+                    arr[k] = b;
                 }
-
+                count++;
             }
-            for (int i = 0; i < arr.length; i++) {
-                System.out.println(arr[i]);
+
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        System.out.println("循环次数：" + count);
+    }
+
+    /**
+     * 冒泡排序
+     *
+     * @param arr
+     */
+    private static void bubbleSort(Integer[] arr) {
+        int temp = 0;
+        int count = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+                count++;
             }
         }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        System.out.println("冒泡循环次数：" + count);
     }
+
+}
 
