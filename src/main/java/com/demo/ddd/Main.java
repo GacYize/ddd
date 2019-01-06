dd495bfc7a392f6b86b0fd169d8f308f421cf7bd
admin <admin@163.com>
2019-01-06 11:39:19 +0800

    777

--- src/main/java/com/demo/ddd/Main.java
+++ src/main/java/com/demo/ddd/Main.java
@@ -1,11 +1,35 @@
 package com.demo.ddd;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(123);
-        System.out.println(122);
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i * 22);
+        Integer[] arr = {1, 23, 53, 243, 2523, 5};
+        Arrays.sort(arr,Collections.reverseOrder());
+        for (Integer a : arr) {
+            System.out.print(a+">");
+        }
+        System.out.println("---------------");
+        chooseSort(arr);
+    }
+
+
+        private static void chooseSort ( Integer[] arr){
+            for (int i = 0; i < arr.length - 1; i++) {
+                for (int k = i + 1; k < arr.length; k++) {
+                    if (arr[i] < arr[k]) {
+                        int b = arr[i];
+                        arr[i] = arr[k];
+                        arr[k] = b;
+                    }
+                }
+
+            }
+            for (int i = 0; i < arr.length; i++) {
+                System.out.println(arr[i]);
+            }
         }
     }
-}
+
